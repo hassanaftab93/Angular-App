4158f39c96ed75b544496334eff77a37dcd292ff
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-10-11 01:30:03 +0500

    Learning components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningMessageComponent,
+    SuccessMessageComponent
   ],
   imports: [
     BrowserModule,
