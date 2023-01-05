443cdc949bd0a8a3f9c7c3412042c38cebaf1302
Hassan Aftab <hassanaftab.pk@gmail.com>
2023-01-06 01:20:32 +0500

    Fix spellings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-App';
+  title = 'My Angular-App';
   name = '';
 }
